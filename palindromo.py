6a09efd88921d7f13e3580a90e1efc9e34a25147
Aquinter10 <124784123+Aquinter10@users.noreply.github.com>
2025-07-28 13:38:39 -0500

    Palindromo Corregido
    Eliminar espacios y signos de puntuacion

--- palindromo.py
+++ palindromo.py
@@ -1,6 +1,17 @@
+import string
+
+def clean_string(sentence):
+    return ''.join(
+        c.lower() for c in sentence if c.isalnum()
+    )
+
 def reverse_string(sentence):
     return sentence[::-1]
-def palindromo(sentence):
-    return sentence == reverse_string(sentence)
 
-print(palindromo("radar"))
+def palindromo(sentence):
+    cleaned = clean_string(sentence)
+    return cleaned == reverse_string(cleaned)
+
+print(palindromo("todo,,.odot"))
+print(palindromo("miel leim"))
+print(palindromo("Muy buenos dias!"))
